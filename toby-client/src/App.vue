5952d697f737159a021b2e891e5ca623b9c35672
mohdfahd <mohammed.f.alsulami@gmail.com>
2024-09-15 13:15:10 +0300

    Add auth to home page page and prevent user to view the home page unless he is logined

--- toby-client/src/App.vue
+++ toby-client/src/App.vue
@@ -1,5 +1,19 @@
-<!-- src/App.vue -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { onMounted } from "vue";
+
+const router = useRouter();
+
+onMounted(() => {
+  // Check if access token exists in localStorage
+  const token = localStorage.getItem("accessToken");
+
+  // If no token is found, redirect to login
+  if (!token) {
+    router.push("/login");
+  }
+});
+</script>
 
 <template>
   <router-view />
